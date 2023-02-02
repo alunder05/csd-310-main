4255f378ac7821a6c85c2bc5aba97c4294763a3e
alunder05 <aunderhill@my365.bellevue.edu>
2023-02-02 16:05:04 -0500

    commit changes

--- Module_8/mysql_test.py
+++ Module_8/mysql_test.py
@@ -1,30 +1,39 @@
+import mysql.connector 
 
-import mysql.connector
-from mysql.connector import errorcode
+from mysql.connector import errorcode 
 
-config = {
-    "user": "pysports_user",
-    "password": "MySQL8IsGreat!",
-    "host": "127.0.0.1",
-    "database": "pysports",
-    "raise_on_warnings": True
-}
+config = { 
 
-try:
-    db = mysql.connector.connect(**config)
+    "user": "pysports_user", 
+    "password": "MySQL8IsGreat!", 
+    "host": "127.0.0.1", 
+    "database": "pysports", 
+    "raise_on_warnings": True 
+} 
 
-    print("\n Database user {} connected to MySQL on host {} with database {}".format(config["user"], config["host"], config["database"]))
-    input("\n\n Press any key to continue...")
 
-except mysql.connector.Error as err:
-    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-        print(" The supplied username or password are invalid")
+try:  
+    db = mysql.connector.connect(**config)  
+    print("\n Database user {} connected to MySQL on host {} with database {}".format(config["user"], config["host"], config["database"]))  
 
-    elif err.errno == errorcode.ER_BAD_DB_ERROR:
-        print(" The specified database does not exist")
+    input("\n\n Press any key to continue...") 
 
-    else:
-        print(err)
+except mysql.connector.Error as err:  
 
-finally:
-    db:close()
+    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:  
+
+ 
+     print(" The supplied username or password are invalid")  
+
+     elif err.errno == errorcode.ER_BAD_DB_ERROR:  
+
+    print(" The specified database does not exist")  
+
+    else:  
+    
+    print(err)  
+
+finally:  
+    db:close()  
+
+ 
