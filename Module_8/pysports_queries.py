531f53d7e57bcd47862f83279a0232428129122a
alunder05 <aunderhill@my365.bellevue.edu>
2023-02-08 11:28:14 -0500

    commit changes

--- Module_8/pysports_queries.py
+++ Module_8/pysports_queries.py
@@ -1,15 +1,21 @@
-""" 
-    Title: pysports_queries.py
-    Author: Professor Krasso
-    Date: 15 July 2020
-    Description: Test program for executing queries against the pysports database. 
-"""
+from copy import error
+from fileinput import close
+import mysql.connector 
 
-""" import statements """
+from mysql.connector import errorcode 
+
+config = { 
+
+    "user": "pysports_user", 
+    "password": "MySQL8IsGreat!", 
+    "host": "127.0.0.1", 
+    "database": "pysports", 
+    "raise_on_warnings": True 
+} 
+
 import mysql.connector
 from mysql.connector import errorcode
 
-""" database config object """
 config = {
     "user": "pysports_user",
     "password": "MySQL8IsGreat!",
